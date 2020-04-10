ccd6af41a1cb898cb9012c935b03ac45a3a24b2b
rabauke79 <63261875+rabauke79@users.noreply.github.com>
2020-04-10 23:28:02 +0200

    Add files via upload

--- arduino-temperature-controlled-fan.ino
+++ arduino-temperature-controlled-fan.ino
@@ -15,9 +15,10 @@
 
 #define DHTPIN 2            // Der Sensor hängt am Arduino am digital Pin2
 #define DHTTYPE DHT22       // Es handelt sich um den DHT22 mit Temperatur- und Luftfeuchtigkeitsensor
-DHT dht(DHTPIN, DHTTYPE);   //sprechen wir den Sensor doch einfach immer mit "dht" an...
+DHT dht(DHTPIN, DHTTYPE);   // sprechen wir den Sensor doch einfach immer mit "dht" an...
 
-const int fan01 = 10;       //Der Ventilator Nummer 1 ist am Arduino am digitalen Pin10 angeschlossen (via MOSFET)
+const int fan01 = 9;       // Der Ventilator Nummer 1 ist am Arduino am digitalen Pin9 (PWM) angeschlossen (via MOSFET)
+const int fan02 = 10;        // Der Ventilator Nummer 2 ist am Arduino am digitalen Pin10 (PWM) angeschlossen (via MOSFET)
 float Temperatur;           // Globale Variale für die Temperatur
 float Luftfeuchtigkeit;     // Globale Variable für die Luftfeuchtigkeit
 bool fanStartImpulsOn = false; // Prüfvariable, ob ich dem Lüfter schon einen kleinen Schubs gegeben habe...
@@ -30,6 +31,7 @@ void setup() {
 
   TCCR1B = TCCR1B & 0b11111000 | 0x01; // Setzt Timer1 (Pin 9 und 10) auf 31300Hz (0x01 kein Pfeifen - 0x03 default)
   pinMode(fan01, OUTPUT); // set pin to output
+  pinMode(fan02, OUTPUT); // set pin to output
 }
 
 //*********************************************************************************************************************
@@ -42,27 +44,55 @@ void loop() {
 
   // Steuerung der Lüftergeschwindigkeit basierend auf der Temperaturauswertung
 
-  if (Temperatur >= 24) {
+  if (Temperatur >= 30) {
     fanSpeed(fan01, 80);
+    fanSpeed(fan02, 80);
+    Serial.println("Sehr warm");
+    Serial.println("");
+  }
+  else if (Temperatur >= 29) {
+    fanSpeed(fan01, 40);
+    fanSpeed(fan02, 40);
+    Serial.println("warm");
+    Serial.println("");
+  }
+  else if (Temperatur >= 27) {
+    fanSpeed(fan01, 30);
+    fanSpeed(fan02, 30);
+    Serial.println("befriedigend warm");
+    Serial.println("");
+  }
+  else if (Temperatur >= 25) {
+    fanSpeed(fan01, 20);
+    fanSpeed(fan02, 20);
+    Serial.println("ausreichend warm");
+    Serial.println("");
   }
   else if (Temperatur >= 23) {
-    fanSpeed(fan01, 10);
+    fanSpeed(fan01, 15);
+    fanSpeed(fan02, 15);
+    Serial.println("mangelhaft warm");
+    Serial.println("");
   }
   else {
-    fanSpeed(fan01,0);
+    fanSpeed(fan01, 0);
+    fanSpeed(fan02, 0);
+    Serial.println("ungenügend warm");
+    Serial.println("");
     fanStartImpulsOn = false;
-    }
+  }
 }
 
 void fanStartImpuls(byte fan) {   /* Kurzer Impuls, um dem Motor einen Startimpuls zu geben */
-  analogWrite(fan, 40);
+  analogWrite(fan, 50);
   delay(250);
   fanStartImpulsOn = true;
 }
 
 void fanSpeed(byte fan, byte setFanSpeed) { /* Übergabe von Werten, mit welcher Geschwindigkeit der Lüfter drehen soll*/
-  if (fanStartImpulsOn == false && setFanSpeed > 0) {
+  if (setFanSpeed > 0 && fanStartImpulsOn == false) {
     fanStartImpuls(fan01);
+    fanStartImpuls(fan02);
   }
   analogWrite(fan, setFanSpeed);
 }
